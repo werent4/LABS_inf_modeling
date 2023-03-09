3743a4ceb6d45394b7c87da180aa2decd1851cab
werent4 <dimavod03@gmail.com>
2023-03-09 23:26:32 +0200

    indexing were added

--- lab_2.py
+++ lab_2.py
@@ -1,16 +1,25 @@
 import os
 documents = os.listdir("documents")
 
+key = 1
+docs_dict = {}
+
+for document in documents:
+    docs_dict[key] = document
+    key +=1
+
+print(docs_dict)
+
 def boolean_search(query):
     result = []
-    for doct in documents:
-        with open(f"documents/{doct}", "r") as d:
+    for doc_key in docs_dict:
+        with open(f"documents/{docs_dict[doc_key]}", "r") as d:
             doc = d.read()
 
         if all(q.lower() in doc.lower() for q in query):
-            result.append(documents.index(doct))
+            result.append(doc_key)
     return result
 
-query = ["rules", "hate"]
+query = ["says Librarian of"]
 
 print(boolean_search(query))
